3132e248dba7cebd50a5a768fbc40d9cfb9df1fe
Thomas Brüggemann <mail@thomasbrueggemann.com>
2022-02-15 20:43:47 +0100

    only check for channel updates on channels that have uploaded a video recently

--- src/repos/channel_repo.rs
+++ src/repos/channel_repo.rs
@@ -37,7 +37,7 @@ impl ChannelRepository {
     pub async fn get_ids_last_crawled_before(
         &self,
         last_crawl_before: chrono::DateTime<Utc>,
-        _last_upload_after: chrono::DateTime<Utc>,
+        last_upload_after: chrono::DateTime<Utc>,
     ) -> Result<Vec<String>, Error> {
         let find_options = FindOptions::builder()
             .projection(doc! { "_id": 1 })
@@ -46,11 +46,17 @@ impl ChannelRepository {
             .build();
 
         let query = doc! {
-            "lastCrawl": {
-                "$lt": mongodb::bson::DateTime::from_millis(
-                    last_crawl_before.timestamp_millis(),
-                )
-            }
+            "$and": [{
+                "lastCrawl": {
+                    "$lt": mongodb::bson::DateTime::from_millis(
+                        last_crawl_before.timestamp_millis(),
+                    )
+                },
+            }, {
+                "lastUpload": {
+                    "$gte": last_upload_after.timestamp()
+                }
+            }]
         };
 
         let cursor = self.collection.find(query, find_options).await?;
